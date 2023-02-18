43fbbd5d8ff9a03222dfa8e16024ad1637da7e7e
jspijker <j.spijker@ultimaker.com>
2023-02-18 14:32:24 +0100

    Added missing includes and API types
    Contributes to CURA-10294

--- include/scripta/logger.h
+++ include/scripta/logger.h
@@ -1,8 +1,15 @@
 #ifndef SCRIPTA_LOGGER_H
 #define SCRIPTA_LOGGER_H
 
+#include <unordered_map>
+
+#include "scripta/section_type.h"
+#include "scripta/visual_data_info.h"
+
 namespace scripta
 {
+
+using layer_map_t = std::unordered_map<int, double>;
 
 constexpr void setAll(auto&&... args){};
 
